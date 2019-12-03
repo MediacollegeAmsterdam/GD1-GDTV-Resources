8447ace119e82f6089886b80145a6c70bce09c8a
SilvanDomino <silvah489@gmail.com>
2019-12-03 12:29:55 +0100

    GDVT Les3 Animations project added

---
+++ GDVT Shooter/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //This number will decide how fast the player will be moving.
+    public float currentSpeed;
+
+    void Update()
+    {
+
+        //check for input. If there's W input, it means the player is moving.
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            currentSpeed = 1;
+        }
+        //Move the player according to input.
+        this.transform.Translate(new Vector3(0, 0, currentSpeed) * Time.deltaTime);
+
+        currentSpeed = 0;
+    }
+}
