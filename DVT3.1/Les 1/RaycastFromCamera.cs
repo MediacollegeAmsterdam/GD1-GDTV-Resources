89dd5560e35b619025a577507a39679ed78d0c45
Silvan Herrema <silvah489@gmail.com>
2020-02-11 09:55:57 +0100

    Update RaycastFromCamera.cs

--- DVT3.1/Les 1/RaycastFromCamera.cs
+++ DVT3.1/Les 1/RaycastFromCamera.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class RaycastFromCamera : MonoBehaviour
 {
-  
-
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+        
+        //Debug the ray so it's visible
         Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
+        
         if (Physics.Raycast(ray, out hit))
         {
-            Transform objectHit = hit.transform;
-
             Debug.Log(hit.point);
             // Do something with the object that was hit by the raycast.
         }
