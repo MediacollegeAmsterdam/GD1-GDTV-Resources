6783849fc520a96632673d9f3d71d22c9403eb6b
SilvanDomino <silvah489@gmail.com>
2020-02-11 09:35:02 +0100

    Added Les 1

---
+++ DVT3.1/Les 1/RaycastFromCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastFromCamera : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+
+            Debug.Log(hit.point);
+            // Do something with the object that was hit by the raycast.
+        }
+    }
+}
